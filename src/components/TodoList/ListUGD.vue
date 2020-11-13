2471a7eb09a8d04772a78cbf32e3eaa55cb60c40
Daniel Seto Dimas <dimasdaniel552@gmail.com>
2020-11-13 09:21:17 +0700

    UGD Vue CLI

---
+++ src/components/TodoList/ListUGD.vue
@@ -0,0 +1,235 @@
+<template>
+    <v-main class="list">
+        <h3 class="text-h3 font-weight-medium mb-5">To Do List UGD</h3>
+        
+        <v-card>
+            <v-card-title>
+                <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Search"
+                    single-line
+                    hide-details
+                ></v-text-field>
+                <v-spacer></v-spacer>
+                <v-btn color="purple" dark @click="dialogSelesai = true" style="margin: 1vh;">
+                    TODO SELESAI
+                </v-btn>
+                <v-btn color="success" dark @click="dialog = true"  style="margin: 1vh;">
+                    Tambah
+                </v-btn>
+            </v-card-title>
+            
+            <v-data-table :headers="headers" :items="todos" :search="search">
+                 <template v-slot:[`item.priority`]="{ item }">
+                    <v-chip v-if="item.priority == 'Penting'"
+                        class="ma-2"
+                        color="red"
+                        label
+                        outlined>
+                        {{ item.priority }}
+                    </v-chip>
+                    <v-chip v-else-if="item.priority == 'Biasa'"
+                        class="ma-2"
+                        color="blue"
+                        label
+                        outlined>
+                        {{ item.priority }}
+                    </v-chip>
+                    <v-chip v-else
+                        class="ma-2"
+                        color="green"
+                        label
+                        outlined>
+                        {{ item.priority }}
+                    </v-chip>
+                </template>
+
+            <template v-slot:[`item.actions`]="{ item }">
+                <v-btn small class="mr-2" @click="editItem(item)">
+                    edit
+                </v-btn>
+                <v-btn small @click="deleteItem(item); indexItem = todos.indexOf(item);">
+                    delete
+                </v-btn>
+            </template>
+            </v-data-table>
+        </v-card>
+
+        <v-dialog v-model="dialog" persistent max-width="600px">
+            <v-card>
+                <v-card-title>
+                    <span class="headline">Form Todo</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-text-field
+                            v-model="formTodo.task"
+                            label="Task"
+                            required
+                        ></v-text-field>
+
+                        <v-select
+                            v-model="formTodo.priority"
+                            :items="['Penting', 'Biasa', 'Tidak penting']"
+                            label="Priority"
+                            required
+                        ></v-select>
+
+                        <v-textarea
+                            v-model="formTodo.note"
+                            label="Note"
+                            required
+                        ></v-textarea>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="cancel">
+                        Cancel
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="save">
+                        Save
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <!-- <v-dialog v-model="dialogDelete" persistent max-width="400px">
+            <v-card>
+                <v-card-title>
+                <span class="headline"><b>Yakin menghapus?</b></span>
+                </v-card-title>
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" text @click="cancelDelete">
+                    TIDAK
+                </v-btn>
+                <v-btn color="red darken-1" text @click="deleteItem(indexItem)">
+                    YA
+                </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog> -->
+
+        <v-dialog v-model="dialogSelesai" persistent max-width="600px">
+            <v-data-table :headers="headers" :items="todosSelesai" :search="search">
+                 <template v-slot:[`item.priority`]="{ item }">
+                    <v-chip v-if="item.priority == 'Penting'"
+                        class="ma-2"
+                        color="red"
+                        label
+                        outlined>
+                        {{ item.priority }}
+                    </v-chip>
+                    <v-chip v-else-if="item.priority == 'Biasa'"
+                        class="ma-2"
+                        color="blue"
+                        label
+                        outlined>
+                        {{ item.priority }}
+                    </v-chip>
+                    <v-chip v-else
+                        class="ma-2"
+                        color="green"
+                        label
+                        outlined>
+                        {{ item.priority }}
+                    </v-chip>
+                </template>
+            </v-data-table>
+        </v-dialog>
+    </v-main>
+</template>
+<script>
+export default {
+    name: "List",
+    data() {
+        return {
+            search: null,
+            dialog: false,
+            dialogDelete: false,
+            dialogSelesai: false,
+            headers: [
+                {
+                    text: "Task",
+                    align: "start",
+                    sortable: true,
+                    value: "task",
+                },
+                { text: "Priority", value: "priority" },
+                { text: "Note", value: "note" },
+                { text: "Actions", value: "actions" },
+            ],
+            todos: [
+                {
+                    task: "bernafas",
+                    priority: "Penting",
+                    note: "huffttt",
+                },
+                {
+                    task: "nongkrong",
+                    priority: "Tidak penting",
+                    note: "bersama tman2",
+                },
+                {
+                    task: "masak",
+                    priority: "Biasa",
+                    note: "masak air 500ml",
+                },
+            ],
+            formTodo: {
+                task: null,
+                priority: null,
+                note: null,
+            },
+            todosSelesai: [
+                
+            ]
+        };
+    },
+    methods: {
+        save() {
+            let i;
+            for(i = 0; i < this.todos.length; i++) {
+                if(this.todos[i].task==this.formTodo.task) {
+                    console.log("i: " + i);
+                    this.todos[i].task = this.formTodo.task;
+                    this.todos[i].priority = this.formTodo.priority;
+                    this.todos[i].note = this.formTodo.note;
+                    this.resetForm();
+                    this.dialog = false;
+                    return;
+                }
+            }
+            this.todos.push(this.formTodo);
+            this.resetForm();
+            this.dialog = false;
+        },
+        cancel() {
+            this.resetForm();
+            this.dialog = false;
+        },
+        cancelDelete() {
+            this.dialogDelete = false;
+        },
+        resetForm() {
+            this.formTodo = {
+                task: null,
+                priority: null,
+                note: null,
+            };
+        },
+        editItem(item) {
+            this.formTodo = item;
+            this.dialog = true;
+        },
+        deleteItem(item){
+            // this.todos.splice(i, 1);
+            // this.dialogDelete = false;
+            this.todosSelesai.push(this.todos.indexOf(this.item));
+            confirm('Yakin ingin menghapus?') && this.todos.splice(this.todos.indexOf(item), 1);
+        },
+    },
+};
+</script>
